e45ee2e6b2fcf3c4b6255934ab25748d3369c80f
martin <m.petrov2018@pmg-pernik.com>
2023-01-23 13:25:37 +0200

    create and all actions created

---
+++ DogsDisplayWebApp/DogsDisplayWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DogsDisplayWebApp.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogsDisplayWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
